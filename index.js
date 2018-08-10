a8b4ea9565cd9f94a95f5855e9d33b39fe9df6db
Don Chatelain <djchatelain@gmail.com>
2018-08-09 19:01:53 -0700

    fixes deployment issue with 401 status on FE

--- index.js
+++ index.js
@@ -22,29 +22,29 @@ mongoose.connect(MONGO_URI, { useNewUrlParser: true });
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use('static', express.static(path.resolve(__dirname, 'build')));
+app.use(express.static(path.resolve(__dirname, 'build')));
 app.use(passport.initialize());
 require('./config/passport')(passport); 
 app.use('/api', apiRoutes);
 
 
 // serve index.html and let front end router handle everything...
-app.get('*', passport.authenticate('jwt', { session: false }), (req, res, next) => {  
-  if (req.url.includes('/api/')) return next(); // ...except /api
-  
-  const token = req.headers.authorization ? req.headers.authorization.split(' ')[1] : null;
-  if (!token) return res.json({ success: false, msg: 'no token provided' });
+app.get('*', (req, res, next) => {  
+  if (req.url.includes('/api/')) next(); // ...except /api
+  res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
 
-  const decoded = jwt.decode(token, secret) || {};
-  User
-    .findOne({ name: decoded.name })
-    .then(user => {
-      if (!user) return res.json({ success: false, msg: 'No user found, auth failed' })
-      res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
-    })
-    .catch(err => {
-      res.json({ success: false, msg: 'encounted error while finding user in db; ' + err });
-    });
+  // const token = req.headers.authorization ? req.headers.authorization.split(' ')[1] : null;
+  // if (!token) return res.json({ success: false, msg: 'no token provided' });
+
+  // const decoded = jwt.decode(token, secret) || {};
+  // User
+  //   .findOne({ name: decoded.name })
+  //   .then(user => {
+  //     if (!user) return res.json({ success: false, msg: 'No user found, auth failed' })
+  //   })
+  //   .catch(err => {
+  //     res.json({ success: false, msg: 'encounted error while finding user in db; ' + err });
+  //   });
 }); 
 
 app.listen(PORT, () => console.log('server up and listening on port', PORT));
